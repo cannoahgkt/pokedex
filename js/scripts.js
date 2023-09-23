f32924480836c8e07b470c1ab8500f3adeb9658d
Can Noah <87754848+Asareff@users.noreply.github.com>
2023-09-23 18:24:43 +0200

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let pokemonList = [
+    {
+        name: "Charizard",
+        height: 1.7,
+        types: ["fire", "flying"]
+    },
+    {
+        name: "Lucario",
+        height:1.2,
+        types: ["steel", "fighting"]
+    },
+    {
+        name: "Tyranitar",
+        height: 6,
+        types: ["dark", "rock"]
+    }
+];
