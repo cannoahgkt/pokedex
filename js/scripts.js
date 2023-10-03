ca69775db6a1802621c10da845e35cc10029686c
Can Noah <87754848+Asareff@users.noreply.github.com>
2023-10-03 13:51:19 +0200

    1.5 Commits (forEach)

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,53 @@
-let pokemonList = [
-    {
-        name: "Charizard",
-        height: 1.7,
-        types: ["fire", "flying"]
-    },
-    {
-        name: "Lucario",
-        height:1.2,
-        types: ["steel", "fighting"]
-    },
-    {
-        name: "Tyranitar",
-        height: 6,
-        types: ["dark", "rock"]
-    }
-];
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {
+            name: "Charizard",
+            height: 1.7,
+            types: ["fire", "flying"]
+        },
+        {
+            name: "Lucario",
+            height:1.2,
+            types: ["steel", "fighting"]
+        },
+        {
+            name: "Tyranitar",
+            height: 6,
+            types: ["dark", "rock"]
+        }
+    ];
 
-for (let i =0; i < pokemonList.length; i++) {
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        if (typeof pokemon === "object" && "name" in pokemon && "height" in pokemon) {
+        pokemonList.push(pokemon);
+        }
+    }
+
+    return {
+        getAll: getAll,
+        add: addEventListener
+    };
+})();
+
+    pokemonRepository.getAll().forEach(function(pokemon) {
+        let pokemonName = pokemon.name;
+        let pokemonHeight = pokemon.height;
+        // If the height is higher than 2, the message will appear, otherwise not
+        if (pokemonHeight > 2) {
+            document.write(pokemonName + " (height: " + pokemonHeight + ") -Wow, that's big!<br>");
+        }
+        else {
+            document.write(pokemonName + " (height: " + pokemonHeight + ")<br>");
+        }
+    });
+
+
+
+/* for (let i =0; i < pokemonList.length; i++) {
     let pokemon = pokemonList[i];
     let pokemonName = pokemon.name;
     let pokemonHeight = pokemon.height;
@@ -28,4 +59,4 @@ for (let i =0; i < pokemonList.length; i++) {
         document.write(pokemonName + " (height: " + pokemonHeight + ")<br>");
     }
 
-}
+} */
